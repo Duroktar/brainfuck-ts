402c155b25cb7580b05c9bd18594d465925aca0e
traBpUkciP <scott.doucet@hotmail.com>
2019-09-08 19:47:58 -0300

    added gh-pages dir structure

--- packages/compiler/src/index.ts
+++
@@ -1,4 +0,0 @@
-export { bf } from './lib/bf'
-export { initialState } from './lib/initialState'
-export { parseProgram } from './lib/parseProgram'
-export { BfOptions, Context, State } from './types'
--- packages/compiler/src/lib/initialState.ts
+++
@@ -1,8 +0,0 @@
-export function initialState(memory: number, chars: string[] = []) {
-    return {
-        tape: new Array(memory).fill(0),
-        tapeIndex: 0,
-        chars: chars,
-        charIndex: 0,
-    };
-}
